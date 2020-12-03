6c3a84d9f4be7a89a8aeec02a8ab74361f4de14b
Rajendra Shrestha <rajendrashr@gmail.com>
2020-12-02 20:38:03 -0800

    Adding activate function to sw file

--- public/sw.js
+++ public/sw.js
@@ -11,7 +11,7 @@ const FILES_TO_CACHE = [
     '/icons/icon-512x512.png',
 ];
 
-//Install
+//Install function
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(function (cache) {
@@ -23,3 +23,24 @@ self.addEventListener('install', function(event) {
     );
     self.skipWaiting();
 });
+
+//Activate function
+self.addEventListener('activate', function(event) {
+    //Check the cached file and delete outdated keys before activating new service worker
+    event.waitUntil(
+        caches.keys().then((keyList) => {
+            return Promise.all(
+                keyList.map(key => {
+                     // if current key does not equal current cache name, delete it
+                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+                        console.log('Old cache data have been deleted!');
+                        return caches.delete(key);
+                    } 
+                })
+            );
+        })
+    );
+    // if any open clients, update to active SW
+  self.clients.claim();
+});
+
