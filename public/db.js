df03bee7753a2bff2dbb88bcde80f214635555f1
Rajendra Shrestha <rajendrashr@gmail.com>
2020-12-02 19:59:45 -0800

    Added files to cache in array and set global variable of db

---
+++ public/db.js
@@ -0,0 +1,2 @@
+let db;
+// create a new db request for a "budget" database.
--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,12 @@
 const CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
-const FILES_TO_CACHE = [];
+const FILES_TO_CACHE = [
+    '/',
+    './index.html',
+    './styles.css',
+    './db.js',
+    '/index.js',
+    './manifest.webmanifest',
+    '/icons/icon-192x192.png',
+    '/icons/icon-512x512.png',
+];
