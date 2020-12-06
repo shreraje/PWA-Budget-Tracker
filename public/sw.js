f47c47cfdc1d6e21f78dbea7c24f4d6d8e3beee6
Rajendra Shrestha <rajendrashr@gmail.com>
2020-12-05 18:42:40 -0800

    Added some final comments for future reference, changed variaiable name, added file path for db.js in index.html

--- public/sw.js
+++ public/sw.js
@@ -1,80 +1,81 @@
-const CACHE_NAME = "static-cache-v2";
+console.log('SW link successful!');
+
+const FILE_CACHE_NAME = "static-cache-v2";
 const DATA_CACHE_NAME = "data-cache-v1";
 const FILES_TO_CACHE = [
-    '/',
-    '/index.html',
-    '/styles.css',
-    '/db.js',
-    '/index.js',
-    '/manifest.webmanifest',
-    '/icons/icon-192x192.png',
-    '/icons/icon-512x512.png',
+  '/',
+  '/index.html',
+  '/styles.css',
+  '/db.js',
+  '/index.js',
+  '/manifest.webmanifest',
+  '/icons/icon-192x192.png',
+  '/icons/icon-512x512.png',
 ];
 
-//Install event function
-self.addEventListener('install', function(event) {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(function (cache) {
-            return cache.addAll(FILES_TO_CACHE);
-        })
-        .catch((err) => {
-            console.log(err);
-        })
-    );
-    self.skipWaiting();
+//Install lifecycle method
+self.addEventListener('install', function (event) {
+  event.waitUntil(
+    caches.open(FILE_CACHE_NAME).then(function (cache) {
+      return cache.addAll(FILES_TO_CACHE);
+    })
+      .catch((err) => {
+        console.log(err);
+      })
+  );
+  self.skipWaiting();
 });
 
 //Activate event function
-self.addEventListener('activate', function(event) {
-    //Check the cached file and delete outdated keys before activating new service worker
-    event.waitUntil(
-        caches.keys().then((keyList) => {
-            return Promise.all(
-                keyList.map(key => {
-                     // if current key does not equal current cache name, delete it
-                    if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-                        console.log('Old cache data have been deleted!');
-                        return caches.delete(key);
-                    } 
-                })
-            );
+self.addEventListener('activate', function (event) {
+  //Check the cached file and delete outdated keys before activating new service worker
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(
+        keyList.map(key => {
+          // if current key does not equal current cache name, delete it
+          if (key !== FILE_CACHE_NAME && key !== DATA_CACHE_NAME) {
+            console.log('Old cache data deleted: ', key);
+            return caches.delete(key);
+          }
         })
-    );
-    // if any open clients, update to active SW
+      );
+    })
+  );
+  // if any open clients, update to active SW
   self.clients.claim();
 });
 
 // Fetch event function
-self.addEventListener("fetch", function(event) {
-    console.log('show fetch event:', event);
-    if (event.request.url.includes("/api/")) {
-      event.respondWith(
-        caches.open(DATA_CACHE_NAME).then(cache => {
+self.addEventListener("fetch", function (event) {
+  console.log('show fetch event:', event);
+
+  //Handle api caching
+  if (event.request.url.includes("/api/")) {
+    return event.respondWith(
+      caches.open(DATA_CACHE_NAME)
+        .then(cache => {
           return fetch(event.request)
             .then(response => {
               // If the response was good, clone it and store it in the cache.
               if (response.status === 200) {
                 cache.put(event.request.url, response.clone());
               }
-
               return response;
             })
             .catch(err => {
-              // Network request failed, try to get it from the cache.
+              // Network request failed, tries to get it from the cache.
               return cache.match(event.request);
             });
         }).catch(err => console.log(err))
-      );
+    );
+  }
 
-      return;
-    }
-
-    event.respondWith(
-        caches.open(CACHE_NAME).then(cache => {
-          return cache.match(event.request).then(response => {
-            return response || fetch(event.request);
-          });
-        })
-      );
-    }); 
-
+  event.respondWith(
+    caches.open(FILE_CACHE_NAME).then(cache => {
+      return cache.match(event.request).then(response => {
+        return response || fetch(event.request);
+      });
+    })
+  );
+}); 
